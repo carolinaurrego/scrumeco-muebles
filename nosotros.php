faa60547c2e3f83c62f46da7d2169d3836222c4f
Carolina Urrego <88674122+carolinaurrego@users.noreply.github.com>
2021-11-08 09:09:30 -0500

    Add files via upload

---
+++ nosotros.php
@@ -0,0 +1,97 @@
+<!DOCTYPE HTML>
+<html lange="en">
+<head> 
+    <link rel="icon" href="imagenes/pata.png">
+    <meta charset="UTF-8">
+    <title>ECO&MUEBLES</title>
+    <link rel="stylesheet" href="css/estilosnosotros.css">
+</head>
+<div class="menu">
+        <ul>
+            <li><a class="active" href="http://localhost/ejer.proyecto%20php/index.html">Inicio</a></li>
+            <li><a href="http://localhost/ejer.proyecto%20php/nosotros.php">Nosotros</a></li>
+            <li><a href="http://localhost/ejer.proyecto%20php/servicios.php">Nuestros servicios</a></li>
+            <li><a href="http://localhost/ejer.proyecto%20php/galeria.php">Galeria</a></li>
+            <li><a href="http://localhost/ejer.proyecto%20php/formulariocontacto.php">Contactenos</a></li>
+           <li><a href="http://localhost/ejer.proyecto%20php/formulariocotizar.php">Cotizar</a></li>
+        </ul>
+        <br>
+        <br>
+        <img  class="logo"src="logos/hogar.png">
+             <div class="titulo">
+            <strong> ECO & MUEBLES</strong>
+            </div> 
+            <p>
+            <hr>
+            <p>
+      </div>
+      <center><img class="sala"src="imagenes/25.jpg"  ></center>
+<body>
+      <h1>¿Porque Nosotros?</h1>
+      <center><img src="imagenes/persona.png"height=></center>
+      <div class="contenedor1">
+        <h2>Mision</h2>
+        <h3>mediante sus distintas áreas de trabajo desea estar comprometida con la calidad 
+          final del. Producto y con el medio ambiente Ofreciendo a nuestros clientes las mejores 
+          inovaciones en restautauracion ,   mano de obra calificada y un  precio justo de forma 
+          tal que se darle una segunda oportunidad a los muebles sea una opción cada vez más acogida 
+          y por ende se mayor el beneficio a nivel ambiental generando desarrollo sostenible en el
+           municipio de Soacha
+      </div>
+      <div class="contenedor2">
+        <h2>Vision</h2>
+        <h3>Tener un crecimiento a nivel nacional en todas nuestras areas de servicio,
+          Cuidando siempre nuestros valores de empresa familiar,Dando siempre un servicio 
+          con las mejores caracteristicas como:
+          calidad,calidez, precio justo,honradez
+      </div>
+      <br>
+      <br>
+      <br>
+      <h1>Nuestro Equipo</h1>
+      <br>
+      <br>
+      <div>
+          <img class="equipo" src="imagenes/reparacionmuebles.jpg"width="550px"height="408px" >
+          <img class="equipo"src="imagenes/reparacionmuebles2.jpg "width="550px"height="408px" >
+      </div>
+      <div class="ne">
+        <h3>Esta constituido por personas idóneas , técnicos, tecnólogos y profesionales capacitados 
+            continuamente
+            para brindar seguridad en cada proceso , ofrecer soporte al cliente en todo momento, basado
+            nuestra línea de valores y fundamentos de la empresa.
+            <br>
+            <br>
+            Algunos aspectos de nuestro trabajo en equipo:
+            <br>
+            <br>
+            <br>
+            Actitud y disponibilidad para la generación de un ambiente de trabajo cordial y amistoso.
+            Claridad y conocimiento por parte de todos los involucrados, de los objetivos y metas planteados.
+            Profundo respeto y sentido de pertenencia del equipo.
+            Valoración y reconocimiento explicito del conocimiento, la experiencia y las habilidades de 
+            todos los miembros del equipo.
+            <br>
+            <br>
+            <br>
+            <br>
+
+            Por último, y como condición propia de cualquier equipo, es indispensable la valoración, 
+            receptividad y apoyo de las ideas, sugerencias y aportes de los otros (actitud abierta).
+        </h3>
+      </div>
+            
+</body>
+<br>
+<br>
+<br>
+   <footer>
+    <div class="marginFooter">
+        <h3>2021  ECO & MUEBLES  Comprometidos con el medio ambiente</h3>
+    </div>
+    <div class="marginFooter">
+        <button><a class="active" href="http://localhost/ejer.proyecto%20php/index.html">Inicio</a></button>
+        <button>Suscribete</button>
+    </div>
+</footer>
+</html>
